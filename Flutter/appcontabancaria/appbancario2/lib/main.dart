6e18d9f9489537c45941618a72c1bf32379f576f
Otávio Luque <37673813+mecoxeco@users.noreply.github.com>
2024-09-09 20:20:29 -0300

    péricles

---
+++ Flutter/appcontabancaria/appbancario2/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'views/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplicação Bancária',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeView(), 
+    );
+  }
+}
