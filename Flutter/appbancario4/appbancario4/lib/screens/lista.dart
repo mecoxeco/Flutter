ed7c27abb822c1c7e22c1e9e8ea8a31683f2d1f4
Otávio Luque <37673813+mecoxeco@users.noreply.github.com>
2024-10-15 21:45:04 -0300

    Update lista.dart

--- Flutter/appbancario4/appbancario4/lib/screens/lista.dart
+++ Flutter/appbancario4/appbancario4/lib/screens/lista.dart
@@ -1,63 +1,67 @@
 import 'package:flutter/material.dart';
 import '../services/transacao_service.dart';
-import 'formulario.dart';
+import 'transacao_form_screen.dart';
 
-class TransacaoListPage extends StatefulWidget {
+class TransacaoListScreen extends StatefulWidget {
   @override
-  _TransacaoListPageState createState() => _TransacaoListPageState();
+  _TransacaoListScreenState createState() => _TransacaoListScreenState();
 }
 
-class _TransacaoListPageState extends State<TransacaoListPage> {
+class _TransacaoListScreenState extends State<TransacaoListScreen> {
+  final TransacaoService _transacaoService = TransacaoService();
   List<dynamic> _transacoes = [];
-
-  void _fetchTransacoes() async {
-    var service = TransacaoService();
-    String jsonResponse = await service.getAll();
-    setState(() {
-      _transacoes = json.decode(jsonResponse);
-    });
-  }
-
-  void _deleteTransacao(String id) async {
-    var service = TransacaoService();
-    await service.deleteById(id);
-    _fetchTransacoes();
-  }
 
   @override
   void initState() {
     super.initState();
-    _fetchTransacoes();
+    _loadTransacoes();
+  }
+
+  Future<void> _loadTransacoes() async {
+    final response = await _transacaoService.getAll();
+    setState(() {
+      _transacoes = jsonDecode(response); // Assumindo que a resposta é uma lista JSON.
+    });
+  }
+
+  void _deleteTransacao(String id) async {
+    await _transacaoService.deleteById(id);
+    _loadTransacoes(); // Recarrega a lista após deletar
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Transações'),
+        title: Text('Lista de Transações'),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => TransacaoForm()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TransacaoFormScreen(onSave: _loadTransacoes)),
+              );
             },
-          ),
+          )
         ],
       ),
-      body: ListView.builder(
-        itemCount: _transacoes.length,
-        itemBuilder: (context, index) {
-          var transacao = _transacoes[index];
-          return ListTile(
-            title: Text(transacao['nome']),
-            subtitle: Text(transacao['valor'].toString()),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () => _deleteTransacao(transacao['id'].toString()),
+      body: _transacoes.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _transacoes.length,
+              itemBuilder: (context, index) {
+                final transacao = _transacoes[index];
+                return ListTile(
+                  title: Text(transacao['nome']),
+                  subtitle: Text('Valor: ${transacao['valor']}'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deleteTransacao(transacao['id']),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
