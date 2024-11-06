64e8c9e9768661900402d0ea0206720cbfb9226d
Otávio Luque <37673813+mecoxeco@users.noreply.github.com>
2024-11-05 22:07:16 -0300

    Update home_screen_test.dart

--- playlistEditor/app/test/widget/home_screen_test.dart
+++ playlistEditor/app/test/widget/home_screen_test.dart
@@ -4,25 +4,20 @@ import 'package:app/screens/home_screen.dart';
 
 void main() {
   testWidgets('HomeScreen has buttons', (WidgetTester tester) async {
-    // Build the widget tree
+
     await tester.pumpWidget(MaterialApp(home: HomeScreen()));
 
-    // Verify if the "Biblioteca de Músicas" button is present
     expect(find.text('Biblioteca de Músicas'), findsOneWidget);
 
-    // Verify if the "Playlists" button is present
     expect(find.text('Playlists'), findsOneWidget);
   });
 
   testWidgets('Navigating to MusicScreen works', (WidgetTester tester) async {
-    // Build the widget tree
-    await tester.pumpWidget(MaterialApp(home: HomeScreen()));
 
-    // Tap the "Biblioteca de Músicas" button
+    await tester.pumpWidget(MaterialApp(home: HomeScreen()));
     await tester.tap(find.text('Biblioteca de Músicas'));
     await tester.pumpAndSettle();
 
-    // Verify that the navigation occurred (MusicScreen)
     expect(find.text('Biblioteca de Músicas'), findsNothing);
   });
 }
